eb502e5e275b9f0227a7cab0ddfb378400fbbd6f
David Lambert <codepenguin@gmail.com>
2015-04-09 21:19:44 -0400

    - Added missing PlayOnEvents.cs file.

---
+++ PlaySharp/PlayOnEvents.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Xml;
+
+namespace PlaySharp
+{
+    public class XmlRequestEventArgs : EventArgs
+    {
+        public string RequestUrl;
+        public XmlDocument Xml;
+    }
+
+    public delegate void XmlRequestEventHandler(object sender, XmlRequestEventArgs e);
+}
